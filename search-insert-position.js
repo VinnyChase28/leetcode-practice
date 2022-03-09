a879aeca7d170aa49f533cc06129c76250259cb9
Vincent <vincentgauthier068@gmail.com>
2022-03-09 08:56:23 -0800

    search-insert-position.js

--- search-insert-position.js
+++ search-insert-position.js
@@ -1,21 +1,32 @@
 var searchInsert = function (nums, target) {
-  //first, lets find the index of the target integer
-  let index = nums.indexOf(target);
-  if (index !== -1) {
-    return index;
-  } else if (target > nums[nums.length - 1]) {
-    console.log(nums.length);
-    return nums.length;
-  } else if (target < nums[0]) {
-    return 0;
-  } else {
-    let i = 0;
-    while (target > nums[i]) {
-      i++;
+  let start = 0,
+    end = nums.length - 1;
+
+  //create a new array that would include the number that was not found in the numbs array
+  let newArray = nums.concat(target);
+  //sort the new array 
+  newArray.sort((a, b) => a - b);
+  // Iterate while start not meets end
+  while (start <= end) {
+    // Find the mid index
+    let mid = Math.floor((start + end) / 2);
+
+    // If element is present at mid, return True
+    if (nums[mid] === target) {
+      console.log(mid);
+      return mid;
     }
-    console.log(i);
-    return i + 1;
+    // Else look in left or right half accordingly
+    else if (nums[mid] < target) {
+      start = mid + 1;
+    } else if (nums[mid] > target) {
+      end = mid - 1;
+    }
   }
+
+  //if target is not found in nums, return the index of where the target would be if we included target in the new array 
+  console.log(newArray.indexOf(target));
+  return newArray.indexOf(target);
 };
 
-searchInsert([1, 3, 5, 6], 2);
+searchInsert([1, 3, 5, 6, 7], 8);
